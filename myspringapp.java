f9a8637690991440ee189c80a9ea9a27cdf67a86
Prajwalbr <106425112+Prajwalbr@users.noreply.github.com>
2022-10-30 02:06:34 +0530

    Create myspringapp.java

---
+++ myspringapp.java
@@ -0,0 +1,51 @@
+package com.praju.myspringapp.entities;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+@Entity
+
+
+public class Myspringapp 
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	private String name;
+	private String mail;
+	private String phone;
+	private String city;
+	
+    
+}
