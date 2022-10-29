9aa445178d7090bcc409af7b11d8d1e466c1990c
Prajwalbr <106425112+Prajwalbr@users.noreply.github.com>
2022-10-30 02:07:17 +0530

    Create myspringapprepository.java

---
+++ myspringapprepository.java
@@ -0,0 +1,7 @@
+package com.praju.myspringapp.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MyspringappRepository extends JpaRepository<myspringapp, Long> {
+
+}
